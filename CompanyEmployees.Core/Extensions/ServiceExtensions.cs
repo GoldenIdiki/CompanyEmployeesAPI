268ebdbd5b161a7bf858be30eb81ad78907f4acc
Golden Idikibiebuma <golden.idiki@gmail.com>
2021-06-18 04:07:29 +0100

    Add project files.

---
+++ CompanyEmployees.Core/Extensions/ServiceExtensions.cs
@@ -0,0 +1,46 @@
+﻿using CompanyEmployees.Contracts;
+using CompanyEmployees.LoggerService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Core.Extensions
+{
+    public static class ServiceExtensions
+    {
+        
+        /// <summary>
+        /// configure CORS - Cross-Origin Resource Sharing
+        /// A mechanism to give or restrict access rights to applications from different domains
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            }
+            );
+
+        /// <summary>
+        /// configure IIS Cnnfiguration
+        /// </summary>
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+
+        /// <summary>
+        /// Configure Nlog
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddScoped<ILoggerManager, LoggerManager>();
+    }
+}
---
+++ Contracts/ILoggerManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CompanyEmployees.Contracts
+{
+    public interface ILoggerManager
+    {
+        void LogInfo(string message);
+        void LogWarn(string message);
+        void LogDebug(string message);
+        void LogError(string message);
+
+    }
+}
